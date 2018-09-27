61eb42fbd6db9e2b94582019d24b7de7ee7b14bc
猪不乐意 <914230482@qq.com>
2018-09-27 09:53:18 +0800

    强制内联CSS

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,8 @@ module.exports = {
       filename: 'index.html'
     }
   },
+  // 强制内联CSS
+  css: { extract: false },
   // 扩展 webpack 配置，使 packages 加入编译
   chainWebpack: config => {
     config.module
