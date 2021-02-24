5ec270041adb3f584d1fab3bd55cd5a13a5e0e7e
严畅 <yanchang@segimail.com>
2021-02-24 11:54:49 +0800

    fix: 引入clickoutside，解决tabindex在ie11不生效

--- vue.config.js
+++ vue.config.js
@@ -8,19 +8,5 @@ module.exports = {
     }
   },
   // 强制内联CSS
-  css: { extract: false },
-  // 扩展 webpack 配置，使 packages 加入编译
-  chainWebpack: config => {
-    config.module
-      .rule('js')
-      .include
-        .add('/packages')
-        .end()
-      .use('babel')
-        .loader('babel-loader')
-        .tap(options => {
-          // 修改它的选项...
-          return options
-        })
-  }
+  css: { extract: false }
 }
