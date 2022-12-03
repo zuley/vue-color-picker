6df96591451489889e85e8cb770dcb52bb3e76c4
zuley <914230482@qq.com>
2022-12-03 12:09:52 +0000

    feat：完整示例

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const color = ref('')
+const headleChangeColor = (val: string) => {
+  console.log('颜色改变', val)
+}
 </script>
 
 <template>
-  <colorPicker />
+  <colorPicker v-model="color" @update:modelValue="headleChangeColor" />
 </template>
--- packages/color-picker/src/index.vue
+++ packages/color-picker/src/index.vue
@@ -6,9 +6,15 @@ export default {
 </script>
 
 <script setup lang="ts">
-// 在 setup() 作用域中执行 (对每个实例皆如此)
+const props = defineProps<{
+  modelValue: string
+}>()
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
 </script>
 
 <template>
   测试组件
+  <div class="add" @click="$emit('update:modelValue', 'ddd')">add</div>
 </template>
